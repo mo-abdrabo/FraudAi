5af0a1c72e777c59ee9522f9290db7017052efe8
mo-abdrabo <113473540+mo-abdrabo@users.noreply.github.com>
2025-12-04 18:36:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,17 +62,31 @@ def load_data():
         st.error("❌ Dataset file not found. Please check the path.")
         return pd.DataFrame()
 
+def patch_model_recursive(model):
+    try:
+        if hasattr(model, "steps"):
+            for _, step in model.steps:
+                patch_model_recursive(step)
+        
+        elif hasattr(model, "estimators_"):
+            for estimator in model.estimators_:
+                patch_model_recursive(estimator)
+                
+        else:
+            if not hasattr(model, "monotonic_cst"):
+                setattr(model, "monotonic_cst", None)
+    except Exception as e:
+        pass # نتجاهل أي أخطاء جانبية
+
 @st.cache_resource
 def load_model():
     try:
         model = joblib.load(MODEL_PATH)
+        patch_model_recursive(model)
         return model
     except FileNotFoundError:
         st.warning("⚠️ Model file not found. Running in UI Demo Mode.")
         return None
-
-df = load_data()
-model = load_model()
 
 # ------------------------------------------------------
 # 🛡️ SIDEBAR & NAVIGATION
@@ -303,3 +317,4 @@ elif selected == "Real-Time Prediction":
 </div>
 </div>
 """, unsafe_allow_html=True)
+
